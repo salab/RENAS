a681543f6d103b360ed80567b753f88b67f3876c
doinaoki <doi.n.ab@m.titech.ac.jp>
2024-08-20 19:01:51 +0900

    Create reproductionLightWeight.sh

---
+++ renas/reproductionLightWeight.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+args=""
+projects="projects/baasbox"
+
+python3 -m renas.repository_analyzer $projects
+python3 -m renas.recommendation $projects
+python3 -m renas.evaluator -sim -pre $args
