b93fb5288ae7fb5f687dd93ab1d29d0b08141107
doinaoki <doi.n.ab@m.titech.ac.jp>
2024-10-06 03:00:43 +0900

    Update execRenas.sh

--- renas/execRenas.sh
+++ renas/execRenas.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 set -e
-args=""
 
 for line in  `cat renas/projects.txt`
 do
-args="${args} projects/${line}"
-python3 -m renas.repository_analyzer $line
-python3 -m renas.recommendation $line
+python3 -m renas.repository_analyzer "projects/${line}"
+python3 -m renas.recommendation "projects/${line}"
 done
 
 #python3 -m renas.evaluator **option** $args
